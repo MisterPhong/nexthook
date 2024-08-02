f97ef6c5877b03f7db5bc0a8c6ca5a49ae4323a0
Misterphongsathon <tosalapaja2@hotmail.com>
2024-08-02 18:54:45 +0700

    otp dialog

--- src/app/(screens)/login/page.tsx
+++ src/app/(screens)/login/page.tsx
@@ -37,10 +37,16 @@ export default async function page() {
         </CardContent>
       </Card>
       <Card elevation={3} className="w-96 p-3 flex justify-center gap-x-2">
-        <Typography variant="subtitle1" fontWeight={400}>
+        <Typography fontWeight={400}>
           Don't have an account?
         </Typography>
-        <LinkMui component={Link} href={routers.signup} fontWeight={600}>
+        <LinkMui
+          component={Link}
+          href={routers.signup}
+          fontWeight={600}
+          fontSize={14}
+          className="mt-[1px]"
+        >
           Sign Up
         </LinkMui>
       </Card>
